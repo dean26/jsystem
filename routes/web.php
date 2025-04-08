b65c082719cdb68bc9eff799c0ee02b27e070ec6
Test <l.przezdziek@gmail.com>
2025-04-08 11:03:35 +0200

    Add order - db routes

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CarController;
 use App\Http\Controllers\WebController;
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\OrderDbController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -25,3 +26,13 @@ Route::get('/cars/start', [CarController::class, 'start']);
 Route::get('/cars/lambo', [CarController::class, 'lambo']);
 Route::get('/order', [WebController::class, 'order']);
 Route::get('/payment', [WebController::class, 'payment']);
+
+Route::prefix('orders-db')->name('orders-db.')->controller(OrderDbController::class)
+->group(function () {
+    Route::get('/', 'index')->name('index');
+    Route::get('create', 'create')->name('create');
+    Route::post('/', 'store')->name('store');
+    Route::get('{id}/edit', 'edit')->name('edit');
+    Route::put('{id}', 'update')->name('update');
+    Route::delete('{id}', 'destroy')->name('destroy');
+});
