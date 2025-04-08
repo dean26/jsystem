35eed8a4ea6592bc2e6a41d9002aa58502bcec09
Test <l.przezdziek@gmail.com>
2025-04-08 11:03:24 +0200

    Add orderdb controller

---
+++ app/Http/Controllers/OrderDbController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class OrderDbController extends Controller
+{
+    public function index()
+    {
+        return view('orders.index');
+    }
+
+    public function create()
+    {
+        return view('orders.index');
+    }
+
+    public function store()
+    {
+        return view('orders.index');
+    }
+
+    public function edit()
+    {
+        return view('orders.index');
+    }
+
+    public function update()
+    {
+        return view('orders.index');
+    }
+
+    public function destroy()
+    {
+        return view('orders.index');
+    }
+}
