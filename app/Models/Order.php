f1b0a71b279a71ee475c8ac4ef1056921544165b
Test <l.przezdziek@gmail.com>
2025-04-08 12:55:59 +0200

    Add new models

---
+++ app/Models/Order.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Order extends Model
+{
+    protected $fillable = [
+        'product_name',
+        'quantity',
+        'price',
+    ];
+
+    public function payments(): HasMany
+    {
+        return $this->hasMany(Payment::class);
+    }
+}
